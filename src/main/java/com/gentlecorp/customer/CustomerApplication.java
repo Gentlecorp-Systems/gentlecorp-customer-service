30fa30ec48eb2abea8431f7a5623c60a3d4d7ff5
Caleb-Script <caleb-script@outlook.de>
2025-01-29 00:08:04 +0100

    update CD/CI

--- src/main/java/com/gentlecorp/customer/CustomerApplication.java
+++ src/main/java/com/gentlecorp/customer/CustomerApplication.java
@@ -1,10 +1,8 @@
 package com.gentlecorp.customer;
 
 import com.gentlecorp.customer.config.ApplicationConfig;
-import com.gentlecorp.customer.config.EnvConfig;
 import com.gentlecorp.customer.dev.DevConfig;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
@@ -32,12 +30,11 @@ import static org.springframework.hateoas.support.WebStack.WEBMVC;
 public class CustomerApplication {
 
 	public static void main(String[] args) {
-		// Schlüsselwerte aus der EnvConfig abrufen
-		String keycloakHost = EnvConfig.get("KEYCLOAK_HOST");
-		String keycloakPort = EnvConfig.get("KEYCLOAK_PORT");
+		// .env-Datei laden
+		Dotenv dotenv = Dotenv.configure().load();
 
-		System.out.printf("KEYCLOAK_HOST: %s%n", keycloakHost != null ? keycloakHost : "localhost");
-		System.out.printf("KEYCLOAK_PORT: %s%n", keycloakPort != null ? keycloakPort : "8080");
+		// Umgebungsvariablen setzen
+		dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
 
 
 		final var app = new SpringApplication(CustomerApplication.class);
